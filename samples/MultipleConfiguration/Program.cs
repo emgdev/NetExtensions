e1df410f2e37573aa0306343668fad529d569277
Renato Golia <renato.golia@educations.com>
2019-12-11 17:04:18 +0100

    MultipleConfiguration

---
+++ samples/MultipleConfiguration/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using Microsoft.Extensions.Configuration;
+using EMG.Extensions.Configuration;
+using System.Threading.Tasks;
+
+namespace MultipleConfiguration
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var hostConfiguration = ConfigureHost();
+
+            var configuration = ConfigureApplication(hostConfiguration, args);
+
+            foreach (var item in configuration.AsEnumerable())
+            {
+                await Console.Out.WriteLineAsync($"{item.Key}='{item.Value}'");
+            }
+        }
+
+        private static IConfiguration ConfigureHost()
+        {
+            var hostConfigurationBuilder = new ConfigurationBuilder();
+
+            hostConfigurationBuilder.AddObject(new
+            {
+                ApplicationName = "EMG Tech Academy Test Project",
+                Environment = "Development"
+            });
+
+            hostConfigurationBuilder.AddEnvironmentVariables("DOTNET_");
+            hostConfigurationBuilder.AddECSMetadataFile();
+
+            var hostConfiguration = hostConfigurationBuilder.Build();
+
+            return hostConfiguration;
+        }
+
+        private static IConfiguration ConfigureApplication(IConfiguration hostConfiguration, string[] arguments)
+        {
+            var environment = hostConfiguration["Environment"];
+
+            var configurationBuilder = new ConfigurationBuilder();
+
+            configurationBuilder.AddConfiguration(hostConfiguration);
+
+            if (environment is "Development")
+            {
+                configurationBuilder.AddUserSecrets<Program>();
+            }
+
+            configurationBuilder.AddJsonFile("appsettings.json", false);
+            configurationBuilder.AddJsonFile($"appsettings.{environment}.json", true);
+
+            configurationBuilder.AddEnvironmentVariables("EMG_");
+
+            configurationBuilder.AddCommandLine(arguments);
+
+            var configuration = configurationBuilder.Build();
+
+            return configuration;
+        }
+    }
+}
