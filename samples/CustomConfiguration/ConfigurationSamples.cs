97194c07c12576f7d158a8e05777b839a2168db6
Renato Golia <renato.golia@educations.com>
2019-12-11 16:01:26 +0100

    CustomConfiguration

---
+++ samples/CustomConfiguration/ConfigurationSamples.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Extensions.Configuration;
+using EMG.Extensions.Configuration;
+using Amazon;
+using Amazon.Extensions.NETCore.Setup;
+
+namespace NetExtensions
+{
+    public static class ConfigurationSamples
+    {
+        public static IConfigurationRoot ParameterStore()
+        {
+            var configurationBuilder = new ConfigurationBuilder();
+
+            configurationBuilder.AddSystemsManager("/emg", new AWSOptions { Region = RegionEndpoint.EUWest1 });
+
+            var configuration = configurationBuilder.Build();
+
+            return configuration;
+        }
+
+        public static IConfigurationRoot ECSContainerMetadata()
+        {
+            var configurationBuilder = new ConfigurationBuilder();
+
+            configurationBuilder.AddECSMetadataFile();
+
+            var configuration = configurationBuilder.Build();
+
+            return configuration;
+        }
+
+        public static IConfigurationRoot SecretsManager()
+        {
+            var configurationBuilder = new ConfigurationBuilder();
+
+            configurationBuilder.AddSecretsManager(region: RegionEndpoint.EUWest1);
+
+            var configuration = configurationBuilder.Build();
+
+            return configuration;
+        }
+
+        public static IConfigurationRoot Objects()
+        {
+            var configurationBuilder = new ConfigurationBuilder();
+
+            configurationBuilder.AddObject(new
+            {
+                Hello = "World",
+                Foo = new
+                {
+                    Bar = "My name"
+                },
+                Ints = new[] { 1, 2 },
+                Foos = new[]
+                {
+                    new { Bar = "Renato" },
+                    new { Bar = "Golia" }
+                }
+            });
+
+            var configuration = configurationBuilder.Build();
+
+            return configuration;
+        }
+    }
+}
--- samples/CustomConfiguration/Program.cs
+++ samples/CustomConfiguration/Program.cs
@@ -1,12 +1,48 @@
 ﻿using System;
+using System.Threading.Tasks;
+using McMaster.Extensions.CommandLineUtils;
+using Microsoft.Extensions.Configuration;
 
-namespace CustomConfiguration
+namespace NetExtensions
 {
     class Program
     {
-        static void Main(string[] args)
+        // dotnet run -- <Demo>
+        // e.g. dotnet run -- FileJson
+        static Task<int> Main(string[] args) => CommandLineApplication.ExecuteAsync<Program>(args);
+
+        [Argument(0, "Demo", "Select the demo to run")]
+        public Demo DemoToRun { get; set; } = Demo.Objects;
+
+        private async Task OnExecuteAsync(CommandLineApplication app)
         {
-            Console.WriteLine("Hello World!");
+            IConfigurationRoot configuration = DemoToRun switch
+            {
+                Demo.ParameterStore => ConfigurationSamples.ParameterStore(),
+
+                Demo.ECSContainerMetadata => ConfigurationSamples.ECSContainerMetadata(),
+
+                Demo.SecretsManager => ConfigurationSamples.SecretsManager(),
+
+                Demo.Objects => ConfigurationSamples.Objects(),
+
+                _ => throw new NotSupportedException()
+            };
+
+            await app.Out.WriteLineAsync($"Printing all configuration received from {DemoToRun}");
+
+            foreach (var item in configuration.AsEnumerable())
+            {
+                await app.Out.WriteLineAsync($"{item.Key}='{item.Value}'");
+            }
         }
+    }
+
+    public enum Demo
+    {
+        ParameterStore,
+        ECSContainerMetadata,
+        SecretsManager,
+        Objects
     }
 }
